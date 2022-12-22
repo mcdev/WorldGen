0fa96bb03d7bd48c5597f8d92f7b4c60fcc41055
mcdev <mattchopin@hotmail.com>
2022-12-22 18:47:26 +0100

    Exports OBJ file for water terrain.

--- Source/helpers.h
+++ Source/helpers.h
@@ -20,3 +20,23 @@ struct Color { uint8_t r, g, b, a; };
 
 struct Vec2 { float x, y; };
 struct Vec3 { float x, y, z; };
+
+Vec3 operator - (const Vec3& i, const Vec3& j) {
+    return { i.x - j.x, i.y - j.y, i.z - j.z };
+}
+
+Vec3 operator / (const Vec3& v, float f) {
+    return { v.x / f, v.y / f, v.z / f };
+}
+
+Vec3 cross(const Vec3& i, const Vec3& j) {
+    return { i.y * j.z - j.y * i.z, i.z * j.x - j.z * i.x, i.x * j.y - j.x * i.y };
+}
+
+float length(const Vec3& v) {
+    return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+Vec3 normalize(const Vec3& v) {
+    return v / length(v);
+}
--- Source/main.cpp
+++ Source/main.cpp
@@ -6,7 +6,7 @@
 #include <cmath>
 #include <stdio.h>
 
-uint8_t raw_encode_altitude(float altitude, float min_height, float max_height) {
+static uint8_t raw_encode_altitude(float altitude, float min_height, float max_height) {
     float raw_altitude = lerp(min_height, max_height, altitude);
     raw_altitude = (raw_altitude - min_height) / (max_height - min_height);
     return 255.0f * clamp(raw_altitude, 0.0f, 1.0f);
@@ -41,6 +41,179 @@ static void tga_write(const char *filename, uint32_t width, uint32_t height, uin
 		}
 	}
 	fclose(fp);
+}
+
+static float average_heights(const std::vector<uint8_t>& data, uint32_t width, uint32_t height, int x0, int y0, int x1, int y1) {
+
+    float count = 0.0f;
+    float h = 0.0f;
+    for(int y = y0; y <= y1; ++y) {
+        for (int x = x0; x <= x1; ++x) {
+            if ((x < 0) || (y < 0) || (x >= width) || (y >= height)) {
+                continue;
+            }
+            ++count;
+            h += data[y + x * width];
+        }
+    }
+
+    return h / count;
+}
+
+static void obj_write(const char* filename, const std::vector<uint8_t>& data, uint32_t width, uint32_t height) {
+    FILE* fp;
+    fopen_s(&fp, filename, "wt");
+    if (fp == NULL) {
+        return;
+    }
+
+    // Create the geometry from the height map.
+    std::vector<float> vertices;
+    vertices.resize(3 * (width + 1) * (height + 1));
+
+    std::vector<float> normals;
+    normals.resize(3 * (width + 1) * (height + 1), 0.0f);
+
+    std::vector<uint32_t> indices;
+    indices.resize(2 * 3 * width * height);
+
+    std::vector<float> uv0;
+    uv0.resize(2 * (width + 1) * (height + 1));
+
+    uint32_t quad = 0;
+    for (uint32_t y = 0; y < height; ++y) {
+        for (uint32_t x = 0; x < width; ++x) {
+
+            // Output vertices.
+            int xl = (int)x - 1;
+            int xr = (int)x + 1;
+            int ya = (int)y - 1;
+            int yb = (int)y + 1;
+
+            uint32_t i0 = x + y * (width + 1);
+            uint32_t i1 = i0 + 1;
+            uint32_t i3 = x + (y + 1) * (width + 1);
+            uint32_t i2 = i3 + 1;
+
+            float z0 = average_heights(data, width, height, xl, ya, x,  y);
+            vertices[3 * i0 + 0] = x / (float)width;
+            vertices[3 * i0 + 1] = z0 / 255.0f;
+            vertices[3 * i0 + 2] = y / (float)height;
+
+            if (x == width - 1) {
+                float z1 = average_heights(data, width, height, x, ya, xr, y);
+                vertices[3 * i1 + 0] = 1.0f;
+                vertices[3 * i1 + 1] = z1 / 255.0f;
+                vertices[3 * i1 + 2] = y / (float)height;
+            }
+
+            if (y == height - 1) {
+                float z3 = average_heights(data, width, height, xl, y, x, yb);
+                vertices[3 * i3 + 0] = x / (float)width;
+                vertices[3 * i3 + 1] = z3 / 255.0f;
+                vertices[3 * i3 + 2] = 1.0f;
+
+                if (x == width - 1) {
+                    float z2 = average_heights(data, width, height, x, y, xr, yb);
+                    vertices[3 * i2 + 0] = 1.0f;
+                    vertices[3 * i2 + 1] = z2 / 255.0f;
+                    vertices[3 * i2 + 2] = 1.0f;
+                }
+            }
+
+            // Output triangle indices.
+            indices[3 * 2 * quad + 0] = i0;
+            indices[3 * 2 * quad + 1] = i3;
+            indices[3 * 2 * quad + 2] = i2;
+            indices[3 * 2 * quad + 3] = i0;
+            indices[3 * 2 * quad + 4] = i2;
+            indices[3 * 2 * quad + 5] = i1;
+
+            // Output texture coordinates.
+            uv0[2 * i0 + 0] = x / (float)width;
+            uv0[2 * i0 + 1] = y / (float)height;
+
+            uv0[2 * i1 + 0] = (x + 1) / (float)width;
+            uv0[2 * i1 + 1] = y / (float)height;
+
+            uv0[2 * i2 + 0] = (x + 1) / (float)width;
+            uv0[2 * i2 + 1] = (y + 1) / (float)height;
+
+            uv0[2 * i3 + 0] = x / (float)width;
+            uv0[2 * i3 + 1] = (y + 1) / (float)height;
+
+            ++quad;
+        }
+    }
+
+    // Compute vertex normals.
+    for (uint32_t f = 0; f < indices.size(); f += 3) {
+        auto i0 = indices[f + 0];
+        auto i1 = indices[f + 1];
+        auto i2 = indices[f + 2];
+
+        Vec3 v0;
+        v0.x = vertices[3 * i0 + 0];
+        v0.y = vertices[3 * i0 + 1];
+        v0.z = vertices[3 * i0 + 2];
+
+        Vec3 v1;
+        v1.x = vertices[3 * i1 + 0];
+        v1.y = vertices[3 * i1 + 1];
+        v1.z = vertices[3 * i1 + 2];
+
+        Vec3 v2;
+        v2.x = vertices[3 * i2 + 0];
+        v2.y = vertices[3 * i2 + 1];
+        v2.z = vertices[3 * i2 + 2];
+
+        Vec3 e01 = v1 - v0;
+        Vec3 e02 = v2 - v0;
+
+        Vec3 n = normalize(cross(e01, e02));
+
+        normals[3 * i0 + 0] += n.x;
+        normals[3 * i0 + 1] += n.y;
+        normals[3 * i0 + 2] += n.z;
+
+        normals[3 * i1 + 0] += n.x;
+        normals[3 * i1 + 1] += n.y;
+        normals[3 * i1 + 2] += n.z;
+
+        normals[3 * i2 + 0] += n.x;
+        normals[3 * i2 + 1] += n.y;
+        normals[3 * i2 + 2] += n.z;
+    }
+
+    for (uint32_t n = 0; n < normals.size(); n += 3) {
+        Vec3 nrm { normals[n + 0], normals[n + 1], normals[n + 2] };
+        nrm = normalize(nrm);
+        normals[n + 0] = nrm.x;
+        normals[n + 1] = nrm.y;
+        normals[n + 2] = nrm.z;
+    }
+
+    fprintf(fp, "# Obj file generateds by wgen.exe.\n");
+
+    // Output the geometry to the file.
+    for (auto v = 0; v < vertices.size(); v += 3) {
+        fprintf(fp, "v %f %f %f\n", vertices[v + 0], vertices[v + 1], vertices[v + 2]);
+    }
+    
+    for (auto vn = 0; vn < normals.size(); vn += 3) {
+        fprintf(fp, "vn %f %f %f\n", normals[vn + 0], normals[vn + 1], normals[vn + 2]);
+    }
+
+    for (auto uv = 0; uv < uv0.size(); uv += 2) {
+        fprintf(fp, "vt %f %f\n", uv0[uv + 0], uv0[uv + 1]);
+    }
+    
+    // OBJ indices start at 1.
+    for (auto i = 0; i < indices.size(); i += 3) {
+        fprintf(fp, "f %d/%d/%d %d/%d/%d %d/%d/%d\n", 1 + indices[i + 0], 1 + indices[i + 0], 1 + indices[i + 0], 1 + indices[i + 1], 1 + indices[i + 1], 1 + indices[i + 1], 1 + indices[i + 2], 1 + indices[i + 2], 1 + indices[i + 2]);
+    }
+
+    fclose(fp);
 }
 
 int main(int argc, char** argv) {
@@ -169,6 +342,9 @@ int main(int argc, char** argv) {
 
     heightmap_name = "heightmap_water_" + std::to_string(amplitude) + ".raw";
     raw_write(heightmap_name.c_str(), raw_heightmap_water);
+
+    std::string obj_name = "heightmap_water_" + std::to_string(amplitude) + ".obj";
+    obj_write(obj_name.c_str(), raw_heightmap_water, w_width, w_height);
 
     // Exports the tiles/height/biomes map.
     std::vector<uint8_t> biomes_map;
